1bffde70d67979c5e6f7cc0195cbabfe103c7fe1
m-watari <watari141@gmail.com>
2022-02-05 18:27:25 +0900

    #ヘッダーメニュー修正

--- betacode/header.php
+++ betacode/header.php
@@ -31,48 +31,39 @@
 
 		<header id="masthead" class="site-header">
 
-			<nav class="navbar navbar-expand-lg navbar-light bg-dark">
-				<div class="container-fluid">
-					<a class="navbar-brand" href="#">
-						<?php
-						the_custom_logo();
-						if (is_front_page() && is_home()) :
-						?>
-							<span class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" class="header-title" rel="home"><?php bloginfo('name'); ?></a></span>
-						<?php
-						else :
-						?>
-							<span class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" class="header-title" rel="home"><?php bloginfo('name'); ?></a></span>
-						<?php endif; ?>
-					</a>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-
-					<div class="collapse navbar-collapse" id="navbarCollapse">
-						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							<li class="nav-item">
-								<?php
-								wp_nav_menu(
-									array(
-										'theme_location' => 'menu-1',
-										'menu_id'        => 'primary-menu',
-									)
-								);
-								?>
-							</li>
-						</ul>
-						<span class="navbar-text">
-							<?php
-							$betacode_description = get_bloginfo('description', 'display');
-							if ($betacode_description || is_customize_preview()) :
-							?>
-								<p class="site-description2">
-									<?php echo $betacode_description;?>
-								</p>
-							<?php endif; ?>
-						</span>
-					</div>
-				</div>
-			</nav>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container-fluid">
+			<a class="navbar-brand" href="#">
+			<?php
+				the_custom_logo();
+				if (is_front_page() && is_home()) :
+				?>
+					<span class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" class="header-title" rel="home"><?php bloginfo('name'); ?></a></span>
+				<?php
+				else :
+				?>
+					<span class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" class="header-title" rel="home"><?php bloginfo('name'); ?></a></span>
+				<?php endif; ?>
+			</a>
+			<button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="navbar-collapse collapse" id="navbarColor01" style="">
+				<!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0"> -->
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+					?>
+				<!-- </ul> -->
+				<!-- <form class="d-flex">
+				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-outline-light" type="submit">Search</button>
+				</form> -->
+			</div>
+			</div>
+		</nav>
 		</header><!-- #masthead -->
