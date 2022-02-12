9bc8171742058704a8b5103ee35cdbcda225a0f1
WatariMamoru <mwatari@obi.sg>
2022-02-12 20:22:01 +0900

    top posts Block List Cereate

--- betacode/su-posts-templates/my-template.php
+++ betacode/su-posts-templates/my-template.php
@@ -1,30 +1,32 @@
-
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+<div class="row row-top row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
     <?php if ($posts->have_posts()) : ?>
             
                 <?php while ($posts->have_posts()) : ?>
                     <?php $posts->the_post(); ?>
-                <div class="col">
+                <div class="col col-top">
                 <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                    </svg>
+                
+                    <?php if (has_post_thumbnail()) { 
+                            echo the_post_thumbnail( array(500) ); 
+                        } else { ?>
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">NoImage</text>
+                        </svg> 
+                    <?php } ?>
 
-                    <div class="card-body">
-                        <p class="card-text"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                            </div>
-                            <small class="text-muted">9 mins</small>
-                        </div>
+
+                    <div class="card-body"><a href="<?php the_permalink(); ?>"><h3 class="postCardTitle"><?php the_title(); ?></h3></a><p class="card-text"><?php
+                            $content = get_the_content();
+                            $content = str_replace(array("\r\n", "\r", "\n"), '', $content);
+                            $content = str_replace('\n', '', mb_substr(strip_tags($content), 0, 100,'UTF-8'));
+                            $backspace = '/[\x08]/';
+                            $content  = preg_replace("/( |　)/", "", $content );
+                            $content = preg_replace ( $backspace , '' , $content );
+                            echo $content.'…… ';
+                        ?><a href="<?php the_permalink(); ?>">more →</a></p>
                     </div>
                 </div>
                 </div>
                 <?php endwhile; ?>
-            
     <?php else : ?>
         <p>Posts not found!</p>
     <?php endif; ?>
